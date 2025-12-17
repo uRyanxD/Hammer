8c98fd0383d1550a79df93e63c8f0add2b1b2b65
uRyanxD <familiarodrigues123ro@gmail.com>
2025-12-17 17:06:39 -0300

    Improve decompile script

--- scripts/decompile.sh
+++ scripts/decompile.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/env bash
 set -e
 
-SRC_DIR="server/src/main/java"
 TEMP_DIR="temp"
 MINECRAFT_VERSION="1.8.8"
 VANILLA_JAR="$TEMP_DIR/$MINECRAFT_VERSION"
 MAPPED_JAR="${VANILLA_JAR}-mapped.jar"
+CLASSES_DIR="$TEMP_DIR/classes"
+DECOMPILE_DIR="$TEMP_DIR/decompiled"
 
-if [ ! -d "$SRC_DIR" ]; then
-  echo "Decompiling mapped jar..."
+if [ ! -d "$CLASSES_DIR" ]; then
+  echo "Extracting classes..."
+
+  mkdir "$CLASSES_DIR"
+  cd "$CLASSES_DIR"
+
+  if ! jar xf "$MAPPED_JAR" net/minecraft/server yggdrasil_session_pubkey.der assets; then
+    echo "Failed to extract classes from mapped jar."
+    exit 1
+  fi
+fi
+
+if [ ! -d "$DECOMPILE_DIR" ]; then
+  echo "Decompiling mapped classes..."
 
   if ! java -jar bin/Vineflower.jar \
         -dgs=1 -hdc=0 -asc=1 -udv=0 -rsy=1 -aoa=1 \
-        "$MAPPED_JAR" "$SRC_DIR"; then
-    echo "Failed to decompile mapped jar."
+        "$CLASSES_DIR" "$DECOMPILE_DIR"; then
+    echo "Failed to decompile mapped classes."
     exit 1
   fi
 fi
